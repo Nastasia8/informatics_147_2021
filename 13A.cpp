429791cca41c19c330e17acc42bad9dd2fe987ac
Sparta <kuloleg52@gmail.com>
2021-10-15 21:10:33 +0300

    Kulichkov Oleg

---
+++ 13A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+float a = 4.1, b = 2.7, x_n = 1.2, x_k = 5.2, x_step = 0.8, y;
+for (float i = x_n; i <= x_k; i++){
+y = a*sqrt(i) - b*log(pow(i, 1/5)) / log(abs(i-1));
+cout << "f("<<i<<") = "<<y<<endl;
+}
+    return 0;
+}
---
+++ 13B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+float y, a = 4.1, b = 2.7;
+float arr[] {1.9, 2.15, 2.34, 2.73, 3.16};
+int size = sizeof(arr)/sizeof(arr[0]);
+for (int i = 0; i < size; i++){
+    y = a*sqrt(arr[i]) - b*pow(log(arr[i]), 5) / abs(log(arr[i]-1));
+    cout << "f("<<arr[i]<<") = "<<y<<endl;
+}
+    return 0;
+}
