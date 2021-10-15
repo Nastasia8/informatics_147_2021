22046e87e93fb141d7d3a58f0372b04fa22f0618
Sparta <kuloleg52@gmail.com>
2021-10-15 21:25:07 +0300

    add files

--- 13B.cpp
+++ 13B.cpp
@@ -7,7 +7,7 @@ float y, a = 4.1, b = 2.7;
 float arr[] {1.9, 2.15, 2.34, 2.73, 3.16};
 int size = sizeof(arr)/sizeof(arr[0]);
 for (int i = 0; i < size; i++){
-    y = a*sqrt(arr[i]) - b*pow(log(arr[i]), 5) / abs(log(arr[i]-1));
+    y = a*sqrt(arr[i]) - b*log(pow(arr[i], 1/5)) / log(abs(arr[i]-1));
     cout << "f("<<arr[i]<<") = "<<y<<endl;
 }
     return 0;
