55b2159cc48e5653e1676eb8d1a4d9db664f382b
Zhenka133 <92075327+Zhenka133@users.noreply.github.com>
2021-10-21 09:35:12 +0300

    Лаб 2.1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-int main()
+float lenght(float x, float y, float z)
 {
-    const float b = 2.5;
-    float arr[] = {1.1, 2.4, 3.6, 1.7, 3.9};   
-    int size = sizeof(arr)/sizeof(arr[0]);
-    float s;
-    for (int i = 0; i < size; i++)
-    {
-        s = pow(b, 3) + pow(arr[i], 3);
-        cout << "f(" << arr[i] << ") = " << (1 + pow(sin(s), 2))/pow(s, 1/3) << endl;
-    }
+    float lenght = sqrt (pow(x,2) + pow(y,2) + pow(z,2));
+    cout << "Длина вектора a равна " << lenght << endl;
+    return lenght;
+}
 
+void bvector (float x1, float y1, float z1, lenght1)
+{
+    float xed = x1/lenght1;
+    float yed = y1/lenght1;
+    float zed = z1/lenght1;
+    cout << "Координаты единичного вектора b: {" << xed << ", " << yed << ", " << zed << "}" << endl;
+}
+
+void sklr (float x1, float x2, float y1, float y2, float z1, float z2)
+{
+    float sklr=x1*x2 + y1*y2 + z1*z2;
+    cout << "Скалярное произведение векторов a и c равно " << sklr << endl;
+}
+
+void vector (float x1, float x2, float y1, float y2, float z1, float z2)
+{
+    float ivector = y1*z2 - y2*z1;
+    float jvector = -1*(x1*z2 - x2*z1);
+    float kvector = x1*y2 - x2*y1;
+    cout << "Векторное произведение a и c равно {" <<  ivector << ", " << jvector << ", " << kvector << "}" << endl;
+}
+
+int main ()
+{
+    const float x1 = 1;
+    const float y1 = 5;
+    const float z1 = 3;
+    float lenght1;
+    lenght1 = lenght(x1, y1, z1);
+    float x2;
+    float y2;
+    float z2;
+    cout << "Введите координаты вектора c через пробел: ";
+    cin >> x2 >> y2 >> z2;
+    cout << "Координаты вектора c {" << x2 << ", " << y2 << ", " << z2 << "}" << endl;
+    bvector(x1, y1, z1, lenght1);
+    sklr (x1, y1, z1, x2, y2, z2);
+    vector (x1, y1, z1, x2, y2, z2);
     return 0;
 }
