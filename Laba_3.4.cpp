9ba562055b4f45d6a620ee0e11f51e99a86c935a
Vitalina9 <vita123.kaz@gmail.com>
2021-10-27 22:34:14 +0300

    Lab_3.4

---
+++ Laba_3.4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int Summa (int, int, int);
+
+int main()
+{
+    int start; int end; int Sum;
+    cout << "Enter the start and end of the range " << endl;
+    cin >> start >> end ;
+    cout << "Sum of even numbers = " << Summa(start, end, Sum) << endl;
+
+    return 0;
+}
+
+//int Summa (int start, int end, int Sum)
+//{
+//    for(int i = start; i <= end; i++)
+//    {
+//        if (i % 2 ==0)
+//        {
+//            Sum +=i;
+//        }
+//        else 
+//        {
+//            continue;
+//        }
+//    }
+//    return Sum;
+//}
+
+int Summa(int start, int end, int Sum)
+{
+    while (start <= end)
+    {
+        Sum +=(start%2==0)?start:0;
+        start += 1;
+    }
+    return Sum;
+}
