4b225af9766caf9a479bf375e808e04fd36b4c10
Sparta <kuloleg52@gmail.com>
2021-12-02 22:19:38 +0300

    changes

--- Laba6/task1-1.cpp
+++ Laba6/task1-1.cpp
@@ -5,31 +5,44 @@
 /*1 способ преобразования матрицы(от пользователя)*/
 using namespace std;
 
-int main (){
-int n, m;
-cout << "Введите размер массива через пробел (NxM): ";
-cin >> n>>m;
-cout << "Введите матрицу\n"; // пользователь вводит матрицу
-int arr[n][m];
-for (int i =0; i < n; i++){
-    for (int j =0; j < m; j++){
-        cin >> arr[i][j];
+void Func(int n, int m)
+{
+    cout << "Введите матрицу\n"; // пользователь вводит матрицу
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
     }
-}
-cout <<"\nВаша матрица\n";
-for (int i =0; i < n; i++){
-    for (int j =0; j < m; j++){
-        cout << arr[i][j]<<'\t';
+    cout << "\nВаша матрица\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << '\t';
+        }
+        cout << endl;
     }
-    cout << endl;
+
+    cout << endl
+         << "Переобразование матрицы\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = m - 1; j >= 0; j--)
+        {
+            cout << arr[i][j] << '\t';
+        }
+        cout << endl;
+    }
 }
 
-cout << endl<<"Переобразование матрицы\n";
-for (int i = 0; i < n; i++){
-    for (int j = m-1; j >= 0; j--){
-        cout << arr[i][j]<<'\t';
-    }
-    cout << endl;
-}
+int main()
+{
+    int n, m;
+    cout << "Введите размер массива через пробел (NxM): ";
+    cin >> n >> m;
+    Func(n, m);
     return 0;
 }
