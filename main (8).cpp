e4db439b5e4e1ecf7409b153629304775f309309
09alex01 <91184503+09alex01@users.noreply.github.com>
2021-12-18 09:38:36 +0300

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+    string name;
+    string color;
+    int age;
+public:
+    Animal(string n, int a, string c): name(n), age(a), color(c) {}
+    string getName() {return name;}
+    virtual void show()=0;
+    virtual string speak()=0;
+
+    
+};
+class Dog: public Animal
+{
+public:
+    Dog(string name, int age, string color, string poroda): Animal(name, age, color)
+    {
+        this->poroda=poroda;
+    }
+    void show() override
+    {
+        
+        cout<<name<<age<<color<<poroda<<endl;
+    }
+    string speak() override
+    {
+        return "gaw gaw";
+    }
+private:
+    string poroda;
+    
+};
+
+class Cat : public Animal
+{
+public:
+    Cat(string name, int age, string color, int year): Animal(name, age, color)
+    {
+        this->year=year;
+    }
+    void show() override
+    {
+        
+        cout<<name<<age<<color<<year<<endl;
+    }
+    string speak() override
+    {
+        return "may may";
+    }
+private:
+    int year;
+    
+};
+
+
+int main()
+{
+    Dog dog("tao", 2, "black", "poroda_1");
+    dog.show();
+    cout<<dog.getName()<<" says "<<dog.speak()<<endl;
+    Cat cat("masa", 3, "grey", 2000);
+    cout<<cat.getName()<<" says "<<cat.speak()<<endl;
+    
+    return 0;
+}
