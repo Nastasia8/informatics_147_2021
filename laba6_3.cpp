3fb72204bf3f519681752fbeba8df129badc02ad
fixat0r <92075357+fixat0r@users.noreply.github.com>
2021-12-20 20:52:03 +0300

    Add files via upload

---
+++ laba6_3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <list>
+#include<string>
+#include <iterator>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter number"<<endl;
+    cin>>number;
+    list <int> result;
+    while (number != 1)
+	{
+		if (number%2 == 0)
+		{	
+			number = number / 2;
+			result.push_back(number);
+		}
+		else
+		{
+			number = (number * 3 + 1)/2;
+			result.push_back(number);
+		}
+    }
+    copy(result.begin(), result.end(), ostream_iterator<int>(cout, " "));
+		cout << endl;
+		result.unique(); 
+    return 0;
+}
