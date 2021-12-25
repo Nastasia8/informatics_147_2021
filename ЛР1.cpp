7ef3da6afbb5a2f22df27449e32764ac2c137a55
nevd19 <92077220+nevd19@users.noreply.github.com>
2021-12-25 15:27:58 +0300

    Add files via upload

---
+++ ЛР1.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <cmath>
+
+using namespace student;
+
+const float a = 0.05; // Константа с плавающей запятой
+const float b = 0.06;
+
+int main(int args, char* argv[]) {
+
+    float x = 0.2;
+    float x_end = 0.95;
+    float x_delta = 0.15;
+
+    while (x < x_end) {
+        cout << (acos((x * x) - (b * b)) / asin((x * x) - (a * a))) << endl;
+        x += x_delta;
+    }
+
+    float xs[5] = {0.15, 0.26, 0.37, 0.48, 0.56};
+
+    for (int i = 0; i <= 5; i++) {
+        cout << (acos((xs[i] * xs[i]) - (b * b)) / asin((xs[i] * xs[i]) - (a * a))) << endl;
+    }
+
+    return 0;
+};
